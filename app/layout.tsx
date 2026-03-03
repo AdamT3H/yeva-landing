f83e669561238058bd1cb3d432c0b62bb83e81d4
AdamT3H <adamogorodnik@gmail.com>
2026-03-03 17:09:23 +0200

    Added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 import localFont from "next/font/local";
 
@@ -44,6 +45,27 @@ export default function RootLayout({
           `}
         >
         {children}
+
+        {/* Meta Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2699363500423395');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
       </body>
     </html>
   );
